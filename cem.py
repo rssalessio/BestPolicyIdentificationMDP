eda96bda4ac91af5a936df86988dc3f86ce659a5
Alessio Russo <russo.alessio@outlook.com>
2022-12-13 15:11:45 +0100

    working methods

---
+++ cem.py
@@ -0,0 +1,191 @@
+from __future__ import annotations
+import numpy as np
+import dirichlet
+from abc import ABC, abstractmethod
+from typing import Callable, List, Tuple
+from math import ceil
+from utils import is_positive_definite, is_symmetric, mean_cov
+
+class Population(ABC):
+    """
+    Population class, used by the CEM method
+    """
+    def __init__(self, dim: int):
+        self._dim = dim
+
+    @property
+    def dim(self) -> int:
+        return self._dim
+
+    @abstractmethod
+    def sample(self, num_points: int) -> List[np.ndarray]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def clone(self) -> Population:
+        raise NotImplementedError
+
+    @abstractmethod
+    def update(self, elite_points: List[Tuple[float, np.ndarray]], *args):
+        raise NotImplementedError
+    
+    @abstractmethod
+    def should_stop(self, threshold: float) -> bool:
+        raise NotImplementedError
+
+
+class DirichletPopulation(Population):
+    """
+    Define a Dirichlet population used by the CEM method
+    Args:
+        coefficients (np.ndarray[float]): coefficients of the dirichlet distribution
+    """    
+    coefficients: np.ndarray
+
+    def __init__(self, coefficients: np.ndarray):
+        super().__init__(coefficients.shape[0])
+        assert len(coefficients.shape) == 1, 'coefficients  a vector'
+
+        self.coefficients = coefficients
+
+    def clone(self) -> DirichletPopulation:
+        return DirichletPopulation(self.coefficients.copy())
+
+    def sample(self, num_points: int) -> List[np.ndarray]:
+        # Sample a number of points from the population
+        return np.random.dirichlet(self.coefficients, size=(num_points,))
+
+    def update(self, elite_results: List[Tuple[float, np.ndarray]], smoothed_update: float = 0.5, regularization: float = 1e-3):
+        # Update the population according to the results
+        results, points = list(zip(*elite_results))
+        new_coeff = dirichlet.mle(np.array(points), method='meanprecision', maxiter=15000)
+        self.coefficients = (1 - smoothed_update) * self.coefficients + smoothed_update * (
+            new_coeff + regularization * np.ones(self.dim))
+
+    def should_stop(self, threshold: float) -> bool:
+        # Replace by the entropy?
+        # For now we check the maximum variance w.r.t. each parameter
+        alpha0 = self.coefficients.sum()
+        tilde_alpha = self.coefficients / alpha0
+        variance = tilde_alpha * (1 - tilde_alpha) / (alpha0 + 1)
+        return np.sqrt(np.max(variance)) < threshold
+
+class GaussianPopulation(Population):
+    """
+    Define a Gaussian population used by the CEM method
+    Args:
+        means (np.ndarray[float]): initial mean vector, of size n
+        covariance (np.ndarray[float]): initial covariance, of shape (n,n)
+    """    
+    means: np.ndarray
+    covariance: np.ndarray
+
+    def __init__(self, means: np.ndarray, covariance: np.ndarray):
+        super().__init__(means.shape[0])
+        assert len(means.shape) <= 1, 'Means should be a scalar or a vector'
+        assert means.shape[0] == covariance.shape[0] and means.shape[0] == covariance.shape[1], 'Means and std_devs should have the same dimensionality'
+        assert is_positive_definite(covariance) and is_symmetric(covariance), 'Covariance needs to be PSD and symmetric'
+
+        self.means = means
+        self.covariance = covariance
+
+    def clone(self) -> GaussianPopulation:
+        return GaussianPopulation(self.means.copy(), self.covariance.copy())
+
+    def sample(self, num_points: int) -> List[np.ndarray]:
+        # Sample a number of points from the population
+        return np.random.multivariate_normal(self.means, cov=self.covariance, size=(num_points,))
+
+    def update(self, elite_results: List[Tuple[float, np.ndarray]], smoothed_update: float = 0.2, regularization: float = 1e-1):
+        # Update the population according to the results
+        results, points = list(zip(*elite_results))
+        new_mean, new_cov = mean_cov(np.array(points))
+        # Smooth update of means and covariance
+        self.means = (1 - smoothed_update) * self.means + smoothed_update * new_mean
+        self.covariance = (1 - smoothed_update) * self.covariance + smoothed_update * (new_cov + regularization * np.eye(self.dim))
+
+    def should_stop(self, threshold: float) -> bool:
+        return np.linalg.norm(self.covariance, ord=2) < threshold
+
+
+def evaluate_population(
+    fun: Callable[[np.ndarray], float],
+    population: Population,
+    num_points: int,
+    elite_fraction: float = 0.2) -> Tuple[Population, List[Tuple[float, np.ndarray]]]:
+    """Evaluate a population on a function and updates the population according to the 
+    best results
+
+    Args:
+        fun (Callable[[np.ndarray], float]): function to evaluate
+        population (Population): population used by cem
+        num_points (int): number of points to evlauate
+        elite_fraction (float, optional): Elite fraction. Defaults to 0.2.
+
+    Returns:
+        Tuple[Population, List[Tuple[float, np.ndarray]]]: first element is the new population, the second element
+        are the best results (sorted)
+    """    
+    assert elite_fraction > 0 and elite_fraction < 1, 'Elite fraction needs to be in (0,1)'
+
+    # Sample population
+    points = population.sample(num_points)
+
+    # Evaluate points
+    results = list(map(fun, points))
+
+    # Sort results
+    sorted_results = sorted(zip(results, points), key=lambda x: x[0])
+
+    # Compute elite population
+    elite_results = sorted_results[-ceil(elite_fraction * num_points):]
+
+    # Update population
+    new_population = population.clone()
+    new_population.update(elite_results)
+
+    return new_population, sorted_results
+
+def optimize(
+    fun: Callable[[np.ndarray], float],
+    population: Population,
+    num_points: int,
+    max_iterations: int = 1000,
+    rtol: float = 1e-4,
+    elite_fraction: float = 0.2,
+    threshold: float = 1e-2) -> Tuple[float, np.ndarray]:
+    """Optimize a function using the CEM method
+
+    Args:
+        fun (Callable[[np.ndarray], float]): function to optimize
+        population (Population): Population to use to optimize the function
+        num_points (int): Number of points to evaluate in each iteration
+        max_iterations (int, optional): maximum number of iterations. Defaults to 1000.
+        rtol (float, optional): relative tolerance. Defaults to 1e-4.
+        elite_fraction (float, optional): Fraction of best results used by the CEM method to update
+            the population. Defaults to 0.2.
+        threshold (float, optional): Stop the algorithm when the population has reached the given threshold
+
+    Returns:
+        Tuple[float, np.ndarray]: Tuple (best results, best parameters)
+    """    
+    assert max_iterations > 0, 'Number of max iterations needs to be positive'
+
+    best_result = -np.infty
+    best_params = None
+
+    for epoch in range(max_iterations):
+        # Evaluate and update population
+        population, results = evaluate_population(fun, population, num_points=num_points, elite_fraction=elite_fraction)
+        _best = results[-1]
+        
+        # Check if the results have improved
+        if _best[0] > best_result:
+            prev_best_result = best_result
+            best_result = _best[0]
+            best_params = _best[1]
+
+        if population.should_stop(threshold):
+            break
+    
+    return best_result, best_params, epoch
--- pgd.py
+++ pgd.py
@@ -5,20 +5,22 @@ from cvxpy.constraints.constraint import Constraint
 from typing import List, Callable, Optional, Tuple
 
 
-def frank_wolfe(
+def pgd(
         n: int,
         x0: npt.NDArray[np.float64],
+        eval_fn: Callable[[npt.NDArray[np.float64]], float],        
         jac: Callable[[npt.NDArray[np.float64]], npt.NDArray[np.float64]],
         build_constraints: Callable[[cp.Variable], List[Constraint]],
+        lr: float,
         max_iter: int = 1000,
-        rtol: float = 1.e-4,
+        rtol: float = 1.e-5,
         callback: Optional[Callable[[npt.NDArray[np.float64], npt.NDArray[np.float64], int ], bool]] = None,
         solver: Optional[str] = None,
         verbose: bool = False
         ) -> Tuple[npt.NDArray[np.float64], float, int]:
-    """Run the Frank Wolfe algorithm to solve a convex constrained problem
+    """Run the Projected gradient descent method
     
-    See also https://en.wikipedia.org/wiki/Frank%E2%80%93Wolfe_algorithm
+    See also https://angms.science/doc/CVX/CVX_PGD.pdf
 
     Args:
         n (int): number of parameters
@@ -27,6 +29,7 @@ def frank_wolfe(
             of the form fun(x) -> List[float]
         build_constraints (Callable[[cp.Variable], List[Constraint]]): Callback to build the constraints. The callback
             should accept an input that is the CVXPY variable to optimize, and should return a list of CVXPY constraints
+        lr (float): learning rate
         max_iter (int, optional): Maximum number of iterations. Defaults to 1000.
         rtol (float, optional): Relative tolerance for stopping the algorithm. Defaults to 1.e-5.
         callback (Optional[Callable[[npt.NDArray[np.float64], npt.NDArray[np.float64], int ], bool]], optional): 
@@ -38,10 +41,10 @@ def frank_wolfe(
     Returns:
         Tuple[npt.NDArray[np.float64], float, int]: _description_
     """    
-    # Construct the Frank-Wolfe parametric problem
+    # Construct the projection
     x = cp.Variable(n)
-    grad_f = cp.Parameter(n)
-    objective = x @ grad_f
+    y = cp.Parameter(n)
+    objective = 0.5 * (cp.norm(x - y, p = 2) ** 2)
     
     constraints = build_constraints(x)
     problem = cp.Problem(cp.Minimize(objective), constraints)
@@ -49,30 +52,50 @@ def frank_wolfe(
 
     # Initialize iterations
     next_x = x0
+    prev_res = None
+    
+    # Golden ratio to decrease the learning rate
+    golden = (1 + 5 ** 0.5) / 2
     for k in range(max_iter):
         current_x = next_x
-
+        
         # Compute gradient
-        x.value = current_x
-        grad_f.value = jac(current_x)
+        grad = jac(current_x)
         
-        # Solve problem
-        res = problem.solve(warm_start=True, verbose=verbose, solver=solver)
-        
-        # Compute next iterate
-        alpha = 2 / (k + 2)
 
-        next_x = current_x + alpha * (x.value - current_x)
+        # Solve problem
+        MAX_NUMBER_INNER_ITERATIONS = max_iter // 10
+        for _i in range(MAX_NUMBER_INNER_ITERATIONS):
+            x.value = current_x
+            y.value = current_x - lr * grad#np.clip(grad, -1e15, 1e15)
+            try:
+                res = problem.solve(warm_start=True, verbose=verbose, solver=solver)
+
+                if prev_res is not None and res > prev_res:                    
+                    lr = lr / golden
+                else: break
+            except Exception as e:
+                lr = lr / golden
+
+        if _i == MAX_NUMBER_INNER_ITERATIONS-1:
+            break
+        if prev_res is not None:
+            if np.isclose(np.abs(res-prev_res), 0, atol=0, rtol=rtol):
+                break
+
+        # Compute next iterate
+        prev_res = res
+        next_x = x.value
 
         if callback is not None:
             if callback(current_x, next_x, k):
                 break
-        
+
         # If solution is close to the previous one, stop iterating
-        if np.isclose(np.linalg.norm(next_x - current_x), 0, rtol=rtol, atol=0):
+        if np.isclose(np.linalg.norm(next_x - current_x, ord=2), 0, rtol=rtol, atol=0):
             break
 
-    return next_x, res, k
+    return next_x, eval_fn(next_x), k
         
         
         

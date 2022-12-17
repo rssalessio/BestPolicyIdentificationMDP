43c070223f87a62ab79b2ee1211a5805f1e06c33
Alessio Russo <russo.alessio@outlook.com>
2022-12-17 13:26:59 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+setup(name = 'BestPolicyIdentificationMDP',
+    packages=find_packages(),
+    version = '0.0.1',
+    description = 'Python library for Best Policy Identification',
+    url = 'https://github.com/rssalessio/BestPolicyIdentificationMDP',
+    author = 'Alessio Russo',
+    author_email = 'alessior@kth.se',
+    install_requires=['numpy', 'scipy', 'cvxpy', 'jax'],
+    license='MIT',
+    zip_safe=False,
+    python_requires='>=3.9',
+)
